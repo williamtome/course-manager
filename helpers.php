d7577e9322301a43a5edf10a0403923db476114a
William <william.tomenh@gmail.com>
2022-05-31 21:39:01 -0300

    fix(helpers): set value in $cursos variable.

--- helpers.php
+++ helpers.php
@@ -3,7 +3,7 @@
 if (!function_exists('view')) {
     function view(string $viewName, array $data = [])
     {
-        $data;
+        $cursos = $data;
         require __DIR__ . '/views/cursos/' . $viewName . '.php';
     }
 }
