f4eb00873d11a1a98bfe7e78084b38527b2e1e9c
William <william.tomenh@gmail.com>
2022-06-01 08:10:06 -0300

    feat(CursosController): create action to store course on database.

--- src/Controllers/CursosController.php
+++ src/Controllers/CursosController.php
@@ -3,14 +3,17 @@
 namespace Alura\Cursos\Controllers;
 
 use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
 use Doctrine\ORM\EntityManagerInterface;
 
 class CursosController
 {
     private $repositorioDeCursos;
+    private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager();
         $this->repositorioDeCursos = $entityManager
             ->getRepository(Curso::class);
     }
@@ -25,5 +28,21 @@ class CursosController
     public function create()
     {
         return view('novo-curso');
+    }
+
+    public function store()
+    {
+        $description = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
+
+        $course = new Curso();
+        $course->setDescricao($description);
+        $this->entityManager->persist($course);
+        $this->entityManager->flush();
+
+        header('Location: /', false, 302);
     }
 }
