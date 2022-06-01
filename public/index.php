4c117e2c2104643da344266d9963e363bd618f91
William <william.tomenh@gmail.com>
2022-05-31 21:33:41 -0300

    refactor(index): improve route.

--- public/index.php
+++ public/index.php
@@ -1,15 +1,21 @@
 <?php
 
-use Alura\Cursos\Controller\CursosController;
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controllers\CursosController;
+use Alura\Cursos\Infra\EntityManagerCreator;
 
 $request = $_SERVER['REQUEST_URI'];
+$entityManager = (new EntityManagerCreator())->getEntityManager();
+$controller = new CursosController($entityManager);
 
-if ($request === '/') {
-    $controller = new CursosController();
-    var_dump($request); die();
-    $controller->index();
-} else if(! $request) {
-    echo "Error 404!";
-} else {
-    require __DIR__ . $request . '.php';
+switch ($request) {
+    case '/':
+        $controller->index();
+        break;
+    case '/novo-curso':
+        $controller->create();
+        break;
+    default:
+        echo "Error 404!";
 }
