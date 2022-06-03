003aa6a0c3c298673ca9240081dd408d8fd711d6
William <william.tomenh@gmail.com>
2022-06-03 20:46:19 -0300

    refactor(index): call the routes to execute controller methods.

--- public/index.php
+++ public/index.php
@@ -2,23 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Alura\Cursos\Controllers\CursosController;
-use Alura\Cursos\Infra\EntityManagerCreator;
+$path = $_SERVER['REQUEST_URI'];
+$routes = require __DIR__ . '/../config/routes.php';
 
-$request = $_SERVER['REQUEST_URI'];
-$entityManager = (new EntityManagerCreator())->getEntityManager();
-$controller = new CursosController($entityManager);
-
-switch ($request) {
-    case '/':
-        $controller->index();
-        break;
-    case '/novo-curso':
-        $controller->create();
-        break;
-    case '/salvar-curso':
-        $controller->store();
-        break;
-    default:
-        echo "Error 404!";
+if (!array_key_exists($path, $routes)) {
+    http_response_code(404);
+    exit();
 }
+
+$controllerClass = $routes[$path][0];
+$action = $routes[$path][1];
+
+$controller = new $controllerClass();
+$controller->{$action}();
