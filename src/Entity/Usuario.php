1ee0e5f162431cdd1499118918a9f692923c4f37
William <william.tomenh@gmail.com>
2022-08-03 09:38:44 -0300

    feat(Usuario): add getters and setters.

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -16,10 +16,35 @@ class Usuario
      * @Column(type="string")
      */
     private $email;
+
     /**
      * @Column(type="string")
      */
     private $senha;
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param string $senha
+     */
+    public function setSenha(string $senha): void
+    {
+        $this->senha = password_hash($senha,PASSWORD_BCRYPT);
+    }
 
     public function senhaEstaCorreta(string $senhaPura): bool
     {
