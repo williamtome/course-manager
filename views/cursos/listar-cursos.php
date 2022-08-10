c13bc267d60e14598314926757a3dfa3fddefa58
William Tome <william.tomenh@gmail.com>
2022-08-10 12:09:50 -0300

    feat: show flash messages on courses list.

--- views/cursos/listar-cursos.php
+++ views/cursos/listar-cursos.php
@@ -6,6 +6,16 @@ require __DIR__ . '/../header.php';
     <h1>Listar cursos</h1>
 </div>
 
+<?php if (isset($_SESSION['message'])) : ?>
+    <div class="alert alert-<?= $_SESSION['message_type']; ?>">
+        <?= $_SESSION['message']; ?>
+    </div>
+<?php
+    unset($_SESSION['message_type']);
+    unset($_SESSION['message']);
+endif;
+?>
+
 <a href="/novo-curso" class="btn btn-primary mb-2">Novo curso</a>
 
 <ul class="list-group">
